735ee4b641a0e02d8487b4c57fdc4d5d8abe2406
shulman pc <neriyashul@gmail.com>
2019-06-04 21:32:16 +0300

    add loadAnddisplay

---
+++ WebApplication/Controllers/DummyController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication.Controllers
+{
+    public class DummyController : ApiController
+    {
+         
+
+
+        [HttpGet]
+        public ActionResult loadAndDisplay(string ip, int port)
+        {
+            IModel model = MyModel.Instance;
+            model.openServer(ip, port);
+
+            Longitude = model.Longitude;
+            Latitude = model.Latitude;
+
+            Session["time"] = 0;
+            return View();
+
+
+            /*InfoModel.Instance.ip = ip;
+            InfoModel.Instance.port = port.ToString();
+            InfoModel.Instance.time = time;
+
+            InfoModel.Instance.ReadData("Dor");
+
+            Session["time"] = time;
+
+
+            return View();*/
+        }
+    }
+}
